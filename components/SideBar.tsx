1372910108814baf16dc4f53b038cdf786a7a977
Liirus91 <work.liirus@gmail.com>
2023-07-09 16:05:05 +0300

    feat: add component Box for sidebar

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -4,6 +4,7 @@ import { usePathname } from 'next/navigation';
 import { useMemo } from 'react';
 import { HiHome } from 'react-icons/hi';
 import { BiSearch } from 'react-icons/bi';
+import Box from './Box';
 
 interface SidebarProps {
   children: React.ReactNode;
@@ -32,7 +33,9 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
 
   return (
     <div className="flex h-full">
-      <div className="hidden md:flex flex-col gap-y-2 bg-black h-full w-[300px] p-2"></div>
+      <div className="hidden md:flex flex-col gap-y-2 bg-black h-full w-[300px] p-2">
+        <Box>Sidebar navigation</Box>
+      </div>
     </div>
   );
 };
