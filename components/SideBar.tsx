bf0f828d7ab4fd0ef3af7405bd550ca08c62663e
Liirus91 <work.liirus@gmail.com>
2023-07-09 16:01:45 +0300

    feat: add routes for sidebar

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,9 +1,40 @@
-interface SideBarProps {
+'use client';
+
+import { usePathname } from 'next/navigation';
+import { useMemo } from 'react';
+import { HiHome } from 'react-icons/hi';
+import { BiSearch } from 'react-icons/bi';
+
+interface SidebarProps {
   children: React.ReactNode;
 }
 
-const SideBar: React.FC<SideBarProps> = ({ children }) => {
-  return <div>{children}</div>;
+const Sidebar: React.FC<SidebarProps> = ({ children }) => {
+  const pathname = usePathname();
+
+  const routes = useMemo(
+    () => [
+      {
+        icon: HiHome,
+        label: 'Home',
+        active: pathname !== '/search',
+        href: '/',
+      },
+      {
+        icon: BiSearch,
+        label: 'Starch',
+        active: pathname === '/search',
+        href: '/search',
+      },
+    ],
+    [pathname]
+  );
+
+  return (
+    <div className="flex h-full">
+      <div className="hidden md:flex flex-col gap-y-2 bg-black h-full w-[300px] p-2"></div>
+    </div>
+  );
 };
 
-export default SideBar;
+export default Sidebar;
