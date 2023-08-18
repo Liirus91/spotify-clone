040e0a49637fa877c0e16d3ac856be739b56f45a
Liirus91 <work.liirus@gmail.com>
2023-08-18 07:58:36 +0300

    feat: create upload modal

---
+++ components/UploadModal.tsx
@@ -0,0 +1,16 @@
+import Modal from './Modal';
+
+const UploadModal = () => {
+  return (
+    <Modal
+      title="Upload modal title"
+      isOpen
+      onChange={() => {}}
+      description="Upload modal description"
+    >
+      Upload Modal
+    </Modal>
+  );
+};
+
+export default UploadModal;
