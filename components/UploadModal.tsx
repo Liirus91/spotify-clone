49d14a1182343c1f48a8130891883a0e7e09c28a
Liirus91 <work.liirus@gmail.com>
2023-08-19 09:19:52 +0300

    feat: add onChange and isOpen for UploadModal

--- components/UploadModal.tsx
+++ components/UploadModal.tsx
@@ -1,14 +1,24 @@
+'use client';
+
+import useUploadModal from '@/hooks/useUploadModal';
 import Modal from './Modal';
 
 const UploadModal = () => {
+  const uploadModal = useUploadModal();
+  const onChange = (open: boolean) => {
+    if (!open) {
+      uploadModal.onClose();
+    }
+  };
+
   return (
     <Modal
-      title="Upload modal title"
-      isOpen
-      onChange={() => {}}
-      description="Upload modal description"
+      title="Add song"
+      isOpen={uploadModal.isOpen}
+      onChange={onChange}
+      description="Upload an mp3 file"
     >
-      Upload Modal
+      Form
     </Modal>
   );
 };
