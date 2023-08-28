8277d615825d48670df900e09bc47f17c6aabc0a
Liirus91 <work.liirus@gmail.com>
2023-08-28 14:42:29 +0300

    feat: finish upload modal

--- components/UploadModal.tsx
+++ components/UploadModal.tsx
@@ -10,12 +10,14 @@ import { toast } from 'react-hot-toast';
 import { useUser } from '@/hooks/useUser';
 import uniqid from 'uniqid';
 import { useSupabaseClient } from '@supabase/auth-helpers-react';
+import { useRouter } from 'next/navigation';
 
 const UploadModal = () => {
   const uploadModal = useUploadModal();
   const [isLoading, setIsLoading] = useState(false);
   const { user } = useUser();
   const supabaseClient = useSupabaseClient();
+  const router = useRouter();
 
   const { register, handleSubmit, reset } = useForm<FieldValues>({
     defaultValues: { author: '', title: '', song: null, image: null },
@@ -65,6 +67,27 @@ const UploadModal = () => {
         setIsLoading(false);
         return toast.error('Failed image upload.');
       }
+
+      const { error: supabaseError } = await supabaseClient
+        .from('songs')
+        .insert({
+          user_id: user.id,
+          title: values.title,
+          author: values.author,
+          image_path: imageData.path,
+          song_path: songData.path,
+        });
+
+      if (supabaseError) {
+        setIsLoading(false);
+        return toast.error(supabaseError.message);
+      }
+
+      router.refresh();
+      setIsLoading(false);
+      toast.success('Song created!');
+      reset();
+      uploadModal.onClose();
     } catch (error) {
       toast.error('Something went wrong');
     } finally {
