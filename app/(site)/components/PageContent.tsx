0d7e0c0ca31fcdddbbd83173dd4badafa7a5c898
Liirus91 <work.liirus@gmail.com>
2023-09-01 08:12:54 +0300

    feat: create song item component

--- app/(site)/components/PageContent.tsx
+++ app/(site)/components/PageContent.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import SongItem from '@/components/SongItem';
 import { Song } from '@/types';
 
 interface PageContentProps {
@@ -13,7 +14,9 @@ const PageContent: React.FC<PageContentProps> = ({ songs }) => {
 
   return (
     <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-8 gap-4 mt-4">
-      Page content
+      {songs.map((song) => (
+        <SongItem key={song.id} data={song} onClick={() => {}} />
+      ))}
     </div>
   );
 };
