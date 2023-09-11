6f178300ac8ffd948a84ecbf5d7c23922d900196
Liirus91 <work.liirus@gmail.com>
2023-09-11 15:42:15 +0300

    feat: add image in Media Item component

--- components/MediaItem.tsx
+++ components/MediaItem.tsx
@@ -1,7 +1,37 @@
 'use client';
 
-const MediaItem = () => {
-  return <div>Media Item</div>;
+import useLoadImage from '@/hooks/useLoadImage';
+import { Song } from '@/types';
+import Image from 'next/image';
+
+interface MediaItemProps {
+  data: Song;
+  onClick?: (id: string) => void;
+}
+
+const MediaItem: React.FC<MediaItemProps> = ({ data, onClick }) => {
+  const imageUrl = useLoadImage(data);
+
+  const handleClick = () => {
+    if (onClick) {
+      return onClick(data.id);
+    }
+  };
+  return (
+    <div
+      onClick={handleClick}
+      className="flex items-center gap-x-3 cursor-pointer w-full hover:bg-neutral-800/50 p-2 rounded-md"
+    >
+      <div className="relative rounded-md min-w-[48px] min-h-[48px] overflow-hidden">
+        <Image
+          fill
+          src={imageUrl || '/images/liked.png'}
+          alt="Media item"
+          className="object-cover"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default MediaItem;
