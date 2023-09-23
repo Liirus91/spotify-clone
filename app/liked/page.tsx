750fc776b40020389caf31d6daf37b5a4a2d49f8
Liirus91 <work.liirus@gmail.com>
2023-09-23 08:35:49 +0300

    feat: create Liked page

---
+++ app/liked/page.tsx
@@ -0,0 +1,36 @@
+import getLikedSongs from '@/actions/getLikedSongs';
+import Header from '@/components/Header';
+import Image from 'next/image';
+
+export const revalidate = 0;
+
+const Liked = async () => {
+  const songs = getLikedSongs();
+
+  return (
+    <div className="h-full w-full overflow-hidden overflow-y-auto bg-neutral-900 rounded-lg">
+      <Header>
+        <div className="mt-20">
+          <div className="flex flex-col md:flex-row items-center gap-x-5">
+            <div className="h-32 w-32 relative lg:h-44 lg:w-44">
+              <Image
+                fill
+                src="/images/liked.png"
+                alt="Playlist"
+                className="object-cover"
+              />
+            </div>
+            <div className="flex flex-col gap-y-2 mt-4 md:mt-0">
+              <p className="hidden md:block font-semibold text-sm">Playlist</p>
+              <h1 className="text-white text-4xl sm:text-5xl lg:text-7xl font-bold">
+                Liked songs
+              </h1>
+            </div>
+          </div>
+        </div>
+      </Header>
+    </div>
+  );
+};
+
+export default Liked;
