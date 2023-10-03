850579b891269bd34f39315144201f8942264749
Liirus91 <work.liirus@gmail.com>
2023-10-03 09:19:39 +0300

    feat: create slider component

--- components/PlayerContent.tsx
+++ components/PlayerContent.tsx
@@ -4,7 +4,9 @@ import { Song } from '@/types';
 import MediaItem from './MediaItem';
 import LikeButton from './LikeButton';
 import { BsPauseFill, BsPlayFill } from 'react-icons/bs';
-import { AiFillStepBackward } from 'react-icons/ai';
+import { AiFillStepBackward, AiFillStepForward } from 'react-icons/ai';
+import { HiSpeakerWave, HiSpeakerXMark } from 'react-icons/hi2';
+import Slider from './Slider';
 
 interface PlayerContentProps {
   song: Song;
@@ -14,6 +16,7 @@ interface PlayerContentProps {
 const PlayerContent: React.FC<PlayerContentProps> = ({ song, songUrl }) => {
   //TODO: fix it
   const Icon = true ? BsPauseFill : BsPlayFill;
+  const VolumeIcon = true ? HiSpeakerXMark : HiSpeakerWave;
 
   return (
     <div className="grid grid-cols-2 h-full md:grid-cols-3">
@@ -39,6 +42,24 @@ const PlayerContent: React.FC<PlayerContentProps> = ({ song, songUrl }) => {
           onClick={() => {}}
           className="text-neutral-400 cursor-pointer hover:text-white transition"
         />
+        <div
+          onClick={() => {}}
+          className="flex items-center justify-center h-10 w-10 rounded-full bg-white p-1 cursor-pointer"
+        >
+          <Icon size={30} className="text-black" />
+        </div>
+        <AiFillStepForward
+          size={30}
+          onClick={() => {}}
+          className="text-neutral-400 cursor-pointer hover:text-white transition"
+        />
+      </div>
+
+      <div className="hidden md:flex w-full pr-2 justify-end">
+        <div className="flex items-center w-[120px] gap-x-2">
+          <VolumeIcon size={30} onClick={() => {}} className="cursor-pointer" />
+          <Slider />
+        </div>
       </div>
     </div>
   );
