f455da1c3a7b0bb5cbcfd8ee251e5f82a15f08a7
Liirus91 <work.liirus@gmail.com>
2023-10-10 09:04:25 +0300

    feat: create loading component

---
+++ app/(site)/loading.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import Box from '@/components/Box';
+
+const Loading = () => {
+  return (
+    <Box className="flex items-center justify-center h-full">Loading...</Box>
+  );
+};
+
+export default Loading;
