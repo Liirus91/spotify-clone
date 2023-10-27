a84c61e7584ba5416e1429d9b0be6a93eff9cd89
Liirus91 <work.liirus@gmail.com>
2023-10-27 09:36:03 +0300

    feat: add subscribeModal in onPlay

--- hooks/useOnPlay.ts
+++ hooks/useOnPlay.ts
@@ -2,15 +2,21 @@ import { Song } from '@/types';
 import usePlayer from './usePlayer';
 import useAuthModal from './useAuthModal';
 import { useUser } from './useUser';
+import useSubscribeModal from './useSubscribeModal';
 
 const useOnPlay = (songs: Song[]) => {
+  const subscriveModal = useSubscribeModal();
   const player = usePlayer();
   const authModal = useAuthModal();
-  const { user } = useUser();
+  const { user, subscription } = useUser();
 
   const onPlay = (id: string) => {
     if (!user) {
       return authModal.onOpen();
+    }
+
+    if (!subscription) {
+      return subscriveModal.onOpen();
     }
 
     player.setId(id);
